5376e4f1b8ed72ce13feb13affc35984319ec372
Edmond <edmondye502@gmail.com>
2018-03-21 14:11:52 -0700

    update task model

--- tasks/models.py
+++ tasks/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible  # only if you need to support Python 2
-class Tasks(models.Model):
-    tasks_text = models.CharField(max_length = 200)
+class Task(models.Model):
+    task_text = models.CharField(max_length = 200)
     completed = models.BooleanField(default = False)
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.tasks_text
+        return self.task_text
 
     def is_completed(self):
     	return self.completed
